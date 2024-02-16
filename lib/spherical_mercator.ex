29339060b583b8b255a80bdae8b735c5d8ef49cb
Marcin Lewandowski <kontakt@marcin-lewandowski.com>
2024-02-16 23:54:12 +0100

    Initial commit

---
+++ lib/spherical_mercator.ex
@@ -0,0 +1,18 @@
+defmodule SphericalMercator do
+  @moduledoc """
+  Documentation for `SphericalMercator`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SphericalMercator.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/spherical_mercator_test.exs
@@ -0,0 +1,8 @@
+defmodule SphericalMercatorTest do
+  use ExUnit.Case
+  doctest SphericalMercator
+
+  test "greets the world" do
+    assert SphericalMercator.hello() == :world
+  end
+end
